1fbbb656e1d82f09fc8cdd9c624f6856f36140a4
Diego Bass <diegoalecsander96@gmail.com>
2019-04-23 22:08:39 -0300

    fist version

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")();
+const http = require("http").Server(express);
+const serverSocket = require("socket.io")(http);
+
+const porta = 8000;
+
+http.listen(porta,
+    () =>
+        console.log("Servidor iniciado em http://localhost:" + porta)
+);
+
+express.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+serverSocket.on(
+    "connection",
+    socket => {
+        console.log("cliente "+ socket.id + " conectado");
+        socket.on(
+            "chat msg", 
+            msg => { 
+                console.log("msg recebida do cliente " + socket.id + ": " + msg );
+                serverSocket.emit("chat msg", msg);
+            }
+        );
+    }
+);
